ec972215491d1321340411165ecbc6a52cf191cf
ablancaa <88708431+ablancaa@users.noreply.github.com>
2022-03-23 12:52:28 +0100

    Cambios en notFound

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,11 +1,9 @@
 <template>
 <SearchBar/>
-    
-        <div class="content">
-        <h1>Not Found</h1>
-        <router-link to="/">Home</router-link>
-        </div>
-   
+<h1>Not Found</h1>
+    <router-link to="/">Home</router-link>
+    <br/><br/><br/><br/><br/><br/><br/>
+    <br/><br/><br/><br/><br/><br/><br/>
 </template>
 
 <script>
@@ -14,29 +12,14 @@ import SearchBar from '@/components/SearchBar.vue'
     export default {
         name: 'NotFound',
          components: {
-
    SearchBar,
-  
  },
     }
 </script>
 
-<style scoped>
-#app {
-font-family: Avenir, Helvetica, Arial, sans-serif;
--webkit-font-smoothing: antialiased;
--moz-osx-font-smoothing: grayscale;
-text-align: center;
-color: #2c3e50;
-display: flex;
-flex-wrap: wrap;
-height: 100%;
-width: 100%;
-max-width: 1280px;
-margin: 0 auto;
-}
-
-.content {
-    margin-bottom: 125px;
+<style>
+.espacio{
+    width: 100%;
+    background-color: aquamarine;
 }
 </style>
